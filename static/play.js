ce8113940dd601590aee046ea0399ce6abedbf97
dxu <dean.xu@asml.com>
2019-12-06 15:19:35 +0800

    fetch langauges

--- static/play.js
+++ static/play.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+  initLanguage();
   let area = document.getElementById("code-area");
   let cmArea = CodeMirror.fromTextArea(area, {
     lineNumbers: true,
   });
 }, false);
+
+function initLanguage() {
+  fetch("api/languages")
+    .then(res => {
+      res.text().then(body => {
+        let langs = JSON.parse(body);
+        let languageCombo = document.getElementById("language-select");
+        Object.keys(langs).forEach(e => {
+          let option = document.createElement("option");
+          option.setAttribute("value", e);
+          option.innerText = e;
+          languageCombo.appendChild(option);
+        })
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
--- web/router.go
+++ web/router.go
@@ -1,8 +1,11 @@
 package web
 
 import (
+	"github.com/XDean/playground/config"
 	"github.com/XDean/playground/web/handler"
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"path/filepath"
 )
 
 func initRouter(e *echo.Echo) {
@@ -12,4 +15,12 @@ func initRouter(e *echo.Echo) {
 
 	socketGroup := e.Group("/socket")
 	socketGroup.GET("/play", handler.SocketPlay)
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Redirect(http.StatusFound, "/index")
+	})
+	e.GET("/index", func(c echo.Context) error {
+		return c.File(filepath.Join(config.Inst.Path.Static(), "play.html"))
+	})
+	e.Static("/static", config.Inst.Path.Static())
 }
